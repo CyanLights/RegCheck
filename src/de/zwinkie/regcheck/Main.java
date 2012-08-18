6d636137fab3a21639b66a30f5ec29fd251ded17
Tuqe <the.ultimate.senator@gmail.com>
2012-08-18 19:29:55 -0400

    Initial commit

---
+++ src/de/zwinkie/regcheck/Main.java
@@ -0,0 +1,19 @@
+package de.zwinkie.regcheck;
+
+import org.bukkit.plugin.java.JavaPlugin;
+/**
+ * 
+ * @author Cyan & Tuqe
+ * @since August 2012
+ */
+public class Main extends JavaPlugin{
+	
+	public void onEnable(){
+		//do something
+	}
+	
+	public void onDisable(){
+		//stop it!
+	}
+	
+}
