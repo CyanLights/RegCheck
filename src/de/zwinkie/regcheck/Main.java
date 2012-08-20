bf3cac9fffee8c1ae6c1e0bfa1346617fceefae6
CyanLights <cyanlights@gmail.com>
2012-08-19 21:35:37 -0400

    Began the main class

--- src/de/zwinkie/regcheck/Main.java
+++ src/de/zwinkie/regcheck/Main.java
@@ -6,6 +6,8 @@ import org.bukkit.plugin.java.JavaPlugin;
  * @author Cyan & Tuqe
  * @since August 2012
  */
+
+// Begin main class
 public class Main extends JavaPlugin{
 	
 	public void onEnable(){
