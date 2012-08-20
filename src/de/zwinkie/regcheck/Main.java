98c85d0c35a61ff2b87e0d5d114a817830a28b74
Tuqe <the.ultimate.senator@gmail.com>
2012-08-20 15:44:45 -0400

    It no longer conflicts with mineshafter! I also moved the single method into the main class.

--- src/de/zwinkie/regcheck/Main.java
+++ src/de/zwinkie/regcheck/Main.java
@@ -1,5 +1,11 @@
 package de.zwinkie.regcheck;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -20,6 +26,11 @@ public class Main extends JavaPlugin{
 	
 	public void onDisable(){
 		//stop it!
+		try {
+		if(in != null)
+			in.close();
+		} catch (IOException e) {}
+		
 		getLogger().info("RegCheck has been disabled.");
 	}
 	
@@ -46,7 +57,7 @@ public class Main extends JavaPlugin{
      	           sender.sendMessage("You must specify a username to lookup.");
      	           return false;
      	        }else{
-     	        	if(WebReader.isRegistered(args[0]))
+     	        	if(isRegistered(args[0]))
      	        		sender.sendMessage("§a"+args[0]+" has registered.");
      	        	else
      	        		sender.sendMessage("§c"+args[0]+" has not registered.");
@@ -58,6 +69,28 @@ public class Main extends JavaPlugin{
     	    return false;
     	} //If this has happened the function will break and return true.
     	return false; 
+    }
+    
+    private static BufferedReader in;
+    
+    public static boolean isRegistered(String in_player)
+    {	
+		try {		
+			URL url = new URL("http://zwinkie.de/search.php?stext="+in_player+"&search=Search&method=OR&forum_id=0&stype=members&order=0");
+			URLConnection connection = url.openConnection();
+			connection.connect();
+			
+			in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			in.skip(15050);
+			char[] copyBuffer = new char[1024];
+	        StringBuffer sb = new StringBuffer();
+	        sb.append(copyBuffer, 0, in.read(copyBuffer, 0, 1024));  
+	        
+	        return sb.toString().contains("profile-link");
+	        
+		} catch (IOException e) {
+			return false;
+		}	
     }
 
 
